39ba7ada3b727e9f08c9f333e7316c90c5b0b8df
McCade Larsen <thepiemaker3.14@gmail.com>
2013-11-15 15:38:20 -0700

    resent update

--- baddie.py
+++ baddie.py
@@ -43,4 +43,47 @@ class Baddie():
         IMG = pygame.image.load("dragon.png")        
         surface.blit(IMG, (self.x, self.y))
         return
+
+class Baddie2():
+
+    def __init__(self,width,height,x,y,color):
+        self.width  = width
+        self.height = height
+        self.x      = x
+        self.y      = y
+        self.new_x  = x
+        self.new_y  = y
+        self.speed  = 3
+        self.color  = color
+        self.alive  = True
+        return
+
+    def tick(self,back_wall,upper_wall,lower_wall,far_wall):
+        self.new_y = self.y + self.speed
+        self.new_x = self.x + random.randint(-1,1)
+        if self.new_y >lower_wall:
+            self.setAlive(False)
+        else:
+            self.y = self.new_y
+        if self.new_x < back_wall:
+            self.new_x = back_wall
+        elif self.new_x + self.width > far_wall:
+            self.new_x = far_wall - self.height
+        self.x = self.new_x
+        return self.alive
+
+    def getAlive(self):
+        return self.alive
+
+    def getDimensions(self):
+        return self.x,self.y,self.width,self.height
+
+    def setAlive(self,alive):
+        self.alive = alive
+    
+    def draw(self, surface):
+        IMG = pygame.image.load("hat_4.png")        
+        surface.blit(IMG, (self.x, self.y))
+        return
         
+
