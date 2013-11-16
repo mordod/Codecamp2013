fc78701c2523224f64ed4c230948336f60b379d6
Gilhahn <18peacokass@washk12.org>
2013-11-15 20:30:18 -0700

    ASD

--- SpaceshipData.py
+++ SpaceshipData.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from spaceship import Spaceship, Spaceship2
+from spaceship import Spaceship, Spaceship2, Spaceship3, Spaceship4
 from baddie import Baddie,Baddie2,Baddie3 
 
 class SpaceshipData:
@@ -21,6 +21,14 @@ class SpaceshipData:
         self.spaceship2_height = 10
         self.spaceship2 = Spaceship2(self.spaceship2_width,self.spaceship2_height,0,(self.height / 2) - 10, (255,255,0))
         self.spaceship2_speed = 10
+        self.spaceship3_width = 20
+        self.spaceship3_height = 10
+        self.spaceship3 = Spaceship(self.spaceship_width,self.spaceship_height,0,(self.height / 2) - 10, (255,255,255))
+        self.spaceship3_speed = 10        
+        self.spaceship4_width = 20
+        self.spaceship4_height = 10
+        self.spaceship4 = Spaceship4(self.spaceship4_width,self.spaceship4_height,0,(self.height / 2) - 10, (255,255,0))
+        self.spaceship4_speed = 10
         self.bullets = []
         self.bullet_width = 5
         self.bullet_height = 10
@@ -43,7 +51,7 @@ class SpaceshipData:
         self.gold_color = (255, 255, 255)
         self.gold_x = 10
         self.gold_y = 60
-        self.IMG = pygame.image.load("background.png")
+        self.Img = pygame.image.load("background.png")
         #self.lives = 30
         #self.lives_color = (255, 255, 255)
         #self.lives_x = 10
@@ -73,7 +81,31 @@ class SpaceshipData:
         if pygame.K_s in keys:
             self.spaceship2.moveDown(self.spaceship2_speed,self.height)
         if pygame.K_SPACE in newkeys:
-            self.bullets.append(self.spaceship2.fire(self.bullet_width,self.bullet_height,self.bullet_color)) 
+            self.bullets.append(self.spaceship2.fire(self.bullet_width,self.bullet_height,self.bullet_color))
+
+        if pygame.K_a in keys:
+            self.spaceship3.moveLeft(self.spaceship3_speed)
+        if pygame.K_d in keys:
+            self.spaceship3.moveRight(self.spaceship3_speed,self.upper_limit)
+        if pygame.K_w in keys:
+            self.spaceship3.moveUp(self.spaceship3_speed)
+        if pygame.K_s in keys:
+            self.spaceship3.moveDown(self.spaceship3_speed,self.height)
+        if pygame.K_SPACE in newkeys:
+            self.bullets.append(self.spaceship3.fire(self.bullet_width,self.bullet_height,self.bullet_color))
+
+        if pygame.K_a in keys:
+            self.spaceship4.moveLeft(self.spaceship4_speed)
+        if pygame.K_d in keys:
+            self.spaceship4.moveRight(self.spaceship4_speed,self.upper_limit)
+        if pygame.K_w in keys:
+            self.spaceship4.moveUp(self.spaceship4_speed)
+        if pygame.K_s in keys:
+            self.spaceship4.moveDown(self.spaceship4_speed,self.height)
+        if pygame.K_SPACE in newkeys:
+            self.bullets.append(self.spaceship4.fire(self.bullet_width,self.bullet_height,self.bullet_color))     
+
+
         if self.kills < 150:
             if random.randint(1, self.frame_rate/9) == 1:
                 self.addBaddie()
@@ -85,8 +117,8 @@ class SpaceshipData:
             if random.randint(1, self.frame_rate/3) == 1:
                 self.addBaddie()
                 
-        #if random.randint(1, self.frame_rate/2) == 1:
-         #   self.addBaddie3()
+        if random.randint(1, self.frame_rate/2) == 1:
+            self.addBaddie3()
 
         for bullet in self.bullets:
             bullet.moveBullet()
@@ -139,7 +171,7 @@ class SpaceshipData:
         return
 
     def draw(self,surface):
-        surfac.blit(self.Img, (self.x, self.y))
+        surface.blit(self.Img, (0, 0))
         score_str = "Dragons Killed: " + str(self.kills)
         self.drawTextLeft(surface, score_str, self.score_color, self.score_x, self.score_y, self.font2)
         gold_str = "Gold: " + str(self.money)
--- spaceship.py
+++ spaceship.py
@@ -9,6 +9,7 @@ class Spaceship():
         self.x      = x
         self.y      = y
         self.color  = color
+        self.Img = pygame.image.load("Untitled.png")
         return
 
     def moveLeft(self, dx):
@@ -43,8 +44,7 @@ class Spaceship():
         return Bullet(width,height,(self.x + self.width) , (self.y + (self.height /2) - (height/2)),color)
     
     def draw(self, surface):
-        rect = pygame.Rect( self.x, self.y, self.width, self.height )
-        pygame.draw.rect(surface, self.color, rect)
+        surface.blit(self.Img, (self.x, self.y))
         return
 
 class Spaceship2():
@@ -55,6 +55,7 @@ class Spaceship2():
         self.x      = x
         self.y      = y
         self.color  = color
+        self.Img = pygame.image.load("Untitled 3.png")
         return
 
     def moveLeft(self, dx):
@@ -89,7 +90,99 @@ class Spaceship2():
         return Bullet(width,height,(self.x + self.width) , (self.y + (self.height /2) - (height/2)),color)
     
     def draw(self, surface):
-        rect = pygame.Rect( self.x, self.y, self.width, self.height )
-        pygame.draw.rect(surface, self.color, rect)
+        surface.blit(self.Img, (self.x, self.y))
         return
         
+class Spaceship3():
+
+    def __init__(self,width,height,x,y,color):
+        self.width  = width
+        self.height = height
+        self.x      = x
+        self.y      = y
+        self.color  = color
+        self.Img = pygame.image.load("Untitled 4.png")
+        return
+
+    def moveLeft(self, dx):
+        self.x -= dx
+        # check the wall
+        if self.x < 0:
+            self.x = 0
+        return
+
+    def moveRight(self, dx, upper_limit):
+        self.x += dx
+        # check the wall
+        if self.x > upper_limit:
+            self.x = upper_limit
+        return
+
+    def moveUp(self, dy):
+        self.y -= dy
+        # check the wall
+        if self.y < 0:
+            self.y = 0
+        return
+
+    def moveDown(self, dy, board_height):
+        self.y += dy
+        # check the wall
+        if self.y > board_height - self.height:
+            self.y = board_height - self.height
+        return
+
+    def fire(self,width,height,color):
+        return Bullet(width,height,(self.x + self.width) , (self.y + (self.height /2) - (height/2)),color)
+    
+    def draw(self, surface):
+        surface.blit(self.Img, (self.x, self.y))
+        return
+
+class Spaceship4():
+
+    def __init__(self,width,height,x,y,color):
+        self.width  = width
+        self.height = height
+        self.x      = x
+        self.y      = y
+        self.color  = color
+        self.Img = pygame.image.load("Untitled 2.png")
+        return
+
+    def moveLeft(self, dx):
+        self.x -= dx
+        # check the wall
+        if self.x < 0:
+            self.x = 0
+        return
+
+    def moveRight(self, dx, upper_limit):
+        self.x += dx
+        # check the wall
+        if self.x > upper_limit:
+            self.x = upper_limit
+        return
+
+    def moveUp(self, dy):
+        self.y -= dy
+        # check the wall
+        if self.y < 0:
+            self.y = 0
+        return
+
+    def moveDown(self, dy, board_height):
+        self.y += dy
+        # check the wall
+        if self.y > board_height - self.height:
+            self.y = board_height - self.height
+        return
+
+    def fire(self,width,height,color):
+        return Bullet(width,height,(self.x + self.width) , (self.y + (self.height /2) - (height/2)),color)
+    
+    def draw(self, surface):
+        surface.blit(self.Img, (self.x, self.y))
+        return
+        
+
